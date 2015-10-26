237ebc35444572eeb3d963b704b6fcac3ac08a9e
Haseeb Alam <in.haseeb@msn.com>
2015-10-26 16:44:07 +0530

    Update wp-config.php

--- wpconfig/files/default/wp-config.php
+++ wpconfig/files/default/wp-config.php
@@ -21,13 +21,13 @@ define('WP_SITEURL','');
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define('DB_NAME', 'wordpress');
+define('DB_NAME', 'wordpress1');
 
 /** MySQL database username */
-define('DB_USER', 'wordpress');
+define('DB_USER', 'wordpress1');
 
 /** MySQL database password */
-define('DB_PASSWORD', 'wordpress');
+define('DB_PASSWORD', 'wordpress1');
 
 /** MySQL hostname */
 define('DB_HOST', '');
